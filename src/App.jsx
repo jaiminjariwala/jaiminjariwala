16221fbfbbece284033d31c45922e064fe14fac3
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-01-03 14:14:47 +0530

    added animation.css file path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Contact from './components/Contact/Contact';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Projects from './components/Projects/Projects';
+import './styles/animations.css';
 
 function App() {
   return (
