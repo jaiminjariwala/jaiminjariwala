ae417df42ca5b40f55717c98a9151b166edaf49c
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-01-07 22:40:28 +0530

    added mouse smooth scrolling using lenis

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,44 @@ import Contact from './components/Contact/Contact';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import './styles/animations.css';
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="container">
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Contact />
-      </main>
-    </div>
-  );
+	useEffect(() => {
+		// lenis for smooth scrolling
+		const lenis = new Lenis({
+			duration: 1.8, // longer duration for extended momentum
+			easing: (t) => 1 - Math.pow(1 - t, 4), // custom easing for smooth inertia
+			smoothWheel: true, // wheel smoothness
+			smoothTouch: false, // disabling for touch
+			wheelMultiplier: 1.3, // slightly more sensitive
+			lerp: 0.08, // linear interpolation for inertia effect
+		});
+
+		function raf(time) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+
+		// Cleanup on component unmount
+		return () => {
+			lenis.destroy();
+		};
+	}, []);
+
+	return (
+		<div className="container">
+			<Header />
+			<main>
+				<Hero />
+				<About />
+				<Contact />
+			</main>
+		</div>
+	);
 }
 
 export default App;
