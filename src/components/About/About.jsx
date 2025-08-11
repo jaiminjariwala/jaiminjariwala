9f0eaed60794748c8de7a2c56240ace73d30ee5d
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-08-11 19:58:52 -0400

    updated bio again

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
             <div className='content-and-image'>
                 <div ref={textRef} className="content">
                     <p className='reveal-text-container'>
-                        I’m passionate about enabling robotic autonomy for both humanoid and space exploration robots — from SLAM and Computer Vision to Reinforcement and Imitation Learning in simulated environments.
+                        I’m passionate about enabling robotic autonomy for both humanoid and space exploration robots — through SLAM, Computer Vision, Reinforcement & Imitation Learning in simulated environments.
                     </p>
                     <p className='reveal-text-container'>
                         Furthermore, I like analyzing data, carrying out insights from it and building Machine Learning models using Pytorch.
