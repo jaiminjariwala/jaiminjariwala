4cb04702e01be1b47aa26a5b0160428d44d66be9
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2026-01-10 10:00:04 -0500

    scaled the fonts

--- src/components/Philosophy/Philosophy.jsx
+++ src/components/Philosophy/Philosophy.jsx
@@ -7,7 +7,13 @@ const Philosophy = () => {
     return (
         <div className="philosophy" id="philosophy">
             <div ref={textRef}>
-                <h1 className="reveal-text-container">Less, but better.</h1>
+                <h1 className="reveal-text-container">
+                    <span className="desktop-text">Less, but better.</span>
+                    <span className="mobile-text">
+                        <span className="line">Less,</span>
+                        <span className="line">but better.</span>
+                    </span>
+                </h1>
             </div>
         </div>
     );
