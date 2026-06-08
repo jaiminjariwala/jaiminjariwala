a8892b60c52fdc34220fb350ee23253436ae22c1
Jaimin Jariwala <jaiminjariwala5@gmail.com>
2026-06-08 11:10:00 -0700

    Scale up projects page to fit viewport without scroll

--- next/src/components/ProjectsPage.jsx
+++ next/src/components/ProjectsPage.jsx
@@ -3,11 +3,11 @@ import ProjectsCurl from "@/components/ProjectsCurl";
 
 const ProjectsPage = () => {
   return (
-    <section className="min-h-screen bg-white text-black">
+    <section className="min-h-screen bg-white text-black" style={{ overflow: "clip" }}>
       <Navbar />
 
       <div
-        className="mx-auto w-full max-w-[689px] pb-[72px]"
+        className="mx-auto w-full max-w-[689px] pb-[28px]"
         style={{
           paddingLeft: "clamp(0px, calc((768px - 100vw) * 9999), 20px)",
           paddingRight: "clamp(0px, calc((768px - 100vw) * 9999), 20px)",
